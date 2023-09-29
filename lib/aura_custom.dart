fa78a39018de9ca07fdabaa86a8f0d4c89ac365f
BatesharXgit <bateshar.xyz@gmail.com>
2023-09-29 19:43:20 +0530

    custom

--- lib/aura_custom.dart
+++ lib/aura_custom.dart
@@ -1,6 +1,94 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 
+class AuraCustomTest extends StatefulWidget {
+  const AuraCustomTest({Key? key, required ScrollController controller, required Color color}) : super(key: key);
+
+  @override
+  State<AuraCustomTest> createState() => _AuraCustomTestState();
+}
+
+class _AuraCustomTestState extends State<AuraCustomTest> {
+  final AudioManager audioManager1 = AudioManager();
+  final AudioManager audioManager2 = AudioManager();
+  double volume1 = 1.0; // Initial volume for audio 1
+  double volume2 = 1.0; // Initial volume for audio 2
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.green.shade400,
+        title: Text("Aura Test"),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            _buildAudioControl(
+              label: 'Heavy Rain',
+              audioManager: audioManager1,
+              volume: volume1,
+              onVolumeChanged: (value) {
+                setState(() {
+                  volume1 = value;
+                });
+                audioManager1.setVolume(value);
+              },
+            ),
+            _buildAudioControl(
+              label: 'Fireplace',
+              audioManager: audioManager2,
+              volume: volume2,
+              onVolumeChanged: (value) {
+                setState(() {
+                  volume2 = value;
+                });
+                audioManager2.setVolume(value);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAudioControl({
+    required String label,
+    required AudioManager audioManager,
+    required double volume,
+    required ValueChanged<double> onVolumeChanged,
+  }) {
+    return Column(
+      children: [
+        Text(label),
+        Slider(
+          value: volume,
+          onChanged: onVolumeChanged,
+        ),
+        ElevatedButton(
+          onPressed: () {
+            audioManager.playAudio('nature/$label.mp3');
+          },
+          child: Text('Play $label'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            audioManager.stopAudio();
+          },
+          child: Text('Stop $label'),
+        ),
+      ],
+    );
+  }
+}
+
 class AudioManager {
   final AudioPlayer _player = AudioPlayer();
 
@@ -21,58 +109,8 @@ class AudioManager {
       print('Error stopping audio: $e');
     }
   }
-}
 
-class AuraCustomTest extends StatefulWidget {
-  const AuraCustomTest(
-      {Key? key, required ScrollController controller, required Color color})
-      : super(key: key);
-
-  @override
-  State<AuraCustomTest> createState() => _AuraCustomTestState();
-}
-
-class _AuraCustomTestState extends State<AuraCustomTest> {
-  final AudioManager audioManager = AudioManager();
-
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        backgroundColor: Colors.green.shade400,
-        title: Text("Aura Test"),
-      ),
-      body: SafeArea(
-        child: Column(
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                audioManager.playAudio('nature/heavyrain.mp3');
-              },
-              child: Text('Play Heavy Rain'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                audioManager.playAudio('nature/fireplace.mp3');
-              },
-              child: Text('Play Fireplace'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                audioManager.stopAudio();
-              },
-              child: Text('Stop Audio'),
-            ),
-          ],
-        ),
-      ),
-    );
+  void setVolume(double volume) {
+    _player.setVolume(volume);
   }
 }
