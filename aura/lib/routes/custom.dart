199ece1a77fb46639b78adfc09fb2716b89ce715
BatesharXgit <bateshar.xyz@gmail.com>
2023-09-30 21:17:39 +0530

    custom added

--- aura/lib/routes/custom.dart
+++ aura/lib/routes/custom.dart
@@ -1,14 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class CustomMixin extends StatefulWidget {
-  const CustomMixin(
-      {super.key, required ScrollController controller, required Color color});
+  const CustomMixin({
+    Key? key,
+    required ScrollController controller,
+    required Color color,
+  }) : super(key: key);
 
   @override
   State<CustomMixin> createState() => _CustomMixinState();
 }
 
 class _CustomMixinState extends State<CustomMixin> {
+  final player = AudioPlayer();
+  bool isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadAssetAudio();
+  }
+
+  Future<void> _loadAssetAudio() async {
+    try {
+      await player.setAsset('assets/nature/creek.mp3');
+      player.setLoopMode(LoopMode.all);
+    } catch (e) {
+      print('Error loading audio: $e');
+    }
+  }
+
+  void _toggleAudioPlayback() {
+    if (isPlaying) {
+      player.pause();
+    } else {
+      player.play();
+    }
+    setState(() {
+      isPlaying = !isPlaying;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,10 +54,22 @@ class _CustomMixinState extends State<CustomMixin> {
       body: SafeArea(
         child: Center(
           child: Column(
-            children: [],
+            children: [
+              ElevatedButton(
+                onPressed: _toggleAudioPlayback,
+                child:
+                    Text(isPlaying ? 'Stop Creek Audio' : 'Play Creek Audio'),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
   }
 }
