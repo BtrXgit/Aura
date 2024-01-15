7fc83e4d3b881f60b3a52bb7b17df78f19053bae
BatesharXgit <121009508+BatesharXgit@users.noreply.github.com>
2024-01-15 23:46:18 +0530

    fixed 3d earth page

--- aura/lib/home.dart
+++ aura/lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:aura/aura_composer.dart';
 import 'package:aura/routes/explore.dart';
 import 'package:aura/routes/homepage/homepage.dart';
+import 'package:aura/routes/pages/planet/planet_page.dart';
 import 'package:aura/routes/tweaks.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
@@ -112,7 +113,7 @@ class HomePageState extends State<HomePage>
               controller: controller,
             ),
             const AuraComposerTest(),
-            const ExploreWorld(
+            const PlanetPage(
                 // controller: controller,
                 ),
             SettingsPage(
